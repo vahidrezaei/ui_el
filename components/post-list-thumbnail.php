17dafed655f4e7de074e5116913a43f47bc14b7f
majidrouhani <majidrouhani.ir@gmail.com>
2024-06-04 17:56:55 +0430

    post-list-thumbnail-N1

--- components/post-list-thumbnail.php
+++ components/post-list-thumbnail.php
@@ -15,16 +15,27 @@
 
 <body>
 
+<?php require "../fun/helper.php"; ?>
+<?php $list = JsonToObject(file_get_contents("http://localhost/frontend/prui/ui_el/api-faker-fa.txt")); ?>
+
 <div class="container maxWidth450 p-0">
 
-    <div class="khadamat-img d-flex justify-content-center">
-            <span class="khadamat-txt align-self-end">
-                <span class="khadamat-color">خدمات خودرو در محل شما</span>
-            </span>
-    </div> <!-- image-khadamat -->
-
+    <?php foreach($list->data as $item){ ?>
+    <a href="#" class="thumbnail-list list-group-item d-flex align-items-center bg-white rounded-4 p-1 mb-3">
+        <div class="thumbnail overflow-hidden">
+            <?php if($item->id !== 1) { ?>
+            <img src="<?=$item->avatar;?>" class="w-100 h-100" alt="">
+            <?php }?>
+        </div>
+        <div class="txt-content overflow-hidden">
+            <h6 class="txt-head text-nowrap"><?=$item->title;?></h6>
+            <p class="txt-detail text-nowrap m-0"><?=$item->subject;?></p>
+        </div>
+    </a> <!--thumbnail-list-->
+    <?php } ?>
 
 </div> <!-- container -->
+
 </body>
 <!-- FOOTER----------------------- -->
 
