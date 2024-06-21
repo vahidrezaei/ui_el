c28f52bd9175acc4ddead893fcd7514830aa6ef3
vahidrezaei <vahidrezaei.sh@gmail.com>
2024-06-21 16:04:37 +0330

    vahid text ci-cl

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
         integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
     <style>
+        body{
+            
+        }
     .list-group a h5 {
         color: maroon;
         font-weight: bold;
